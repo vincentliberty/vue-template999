c5eadc627312100a7cb897d924c683d1e983223f
vincentlli <vincentlli@tencent.com>
2025-01-17 16:52:13 +0800

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     proxy: {   
         '/api': {    //这里的/api表示的意思是以/api开头的才生效 ->刷下面的重点
             target: "http://localhost:8088",
-            changOrigin: true,   //如果接口跨域这里就要这个参数配置
+            changeOrigin: true,   //如果接口跨域这里就要这个参数配置
         }
    }
   }
